86e3ddd038bcc395db9cbbf9cc8a30062d25c446
Gabriel Gineste <30419208+gabrielgineste@users.noreply.github.com>
2019-03-17 21:52:12 -0300

    Corrigindo Bugs e fazendo o programa funcionar antes de personalizar e adcionar mais conteúdo.

--- content.py
+++ content.py
@@ -1,9 +1,10 @@
 import fresh_tomatoes
 import constructors
 
-clube_da_luta = constructors.Movie("Clube da Luta",
+clube_da_luta = constructors.Movie("Clube da Luta","2 Horas",
                         "The two bored men form an underground club with strict rules and fight other men who are fed up with their mundane lives.",
                         "https://ae01.alicdn.com/kf/HTB1vzb3NFXXXXbiXFXXq6xXFXXXC/Vintage-Retro-Papel-poster-anime-clube-da-Luta-Brad-Pitt-Cartaz-Cartazes-cartaz-cudi-adesivo-De.jpg",
                         "https://www.youtube.com/watch?v=KUJOXy0e-kE")
 
 movies = [clube_da_luta]
+fresh_tomatoes.open_movies_page(movies)
