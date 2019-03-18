ecd4392c6143e6a88cbc3421c4a7a4ab963927c8
Gabriel Gineste <30419208+gabrielgineste@users.noreply.github.com>
2019-03-17 21:44:16 -0300

    Adcionando os arquivos com o construtor e também aonde será colocado os objetos.

---
+++ constructors.py
@@ -0,0 +1,27 @@
+import webbrowser
+
+class Video():
+    #Essa classe construtora irá criar toda a base para ser adcionado os filmes mais tarde.
+    def __init__(self , title, duration):
+        self.title = title
+        self.duration = duration
+
+class Movie(Video):
+    def __init__(self,title,duration,movie_storyline,poster_image,trailer_youtube):
+        Video.__init__(self,title,duration)
+        self.movie_storyline = movie_storyline
+        self.poster_image = poster_image
+        self.trailer_youtube = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open (self.trailer_youtube)
+
+
+
+class Tvshow(Video):
+    def __init__(self,title,duration,season,number_of_episodes,tv_station):
+        Video.__init__(self, title, duration)
+        self.season = season
+        self.number_of_episodes = number_of_episodes
+        self.tv_station = tv_station
+
---
+++ content.py
@@ -0,0 +1,9 @@
+import fresh_tomatoes
+import constructors
+
+clube_da_luta = constructors.Movie("Clube da Luta",
+                        "The two bored men form an underground club with strict rules and fight other men who are fed up with their mundane lives.",
+                        "https://ae01.alicdn.com/kf/HTB1vzb3NFXXXXbiXFXXq6xXFXXXC/Vintage-Retro-Papel-poster-anime-clube-da-Luta-Brad-Pitt-Cartaz-Cartazes-cartaz-cudi-adesivo-De.jpg",
+                        "https://www.youtube.com/watch?v=KUJOXy0e-kE")
+
+movies = [clube_da_luta]
